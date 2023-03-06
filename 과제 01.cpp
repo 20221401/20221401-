96f681e758e86ac717c510a3ed2afc79acaffa3c
20221401 <127109886+20221401@users.noreply.github.com>
2023-03-07 00:14:32 +0900

    Add files via upload

---
+++ 과제 01.cpp
@@ -0,0 +1,23 @@
+﻿//과제 01.
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string myMbti = "ISFJ";  // 자신의 MBTI 유형
+    string filename = "mbti.txt";
+    ifstream mbtiFile(filename);  // MBTI 데이터 파일 열기
+
+
+    string name, mbti;
+    cout << "Friends with " << myMbti << " MBTI:" << endl;
+    if (mbti == myMbti && name != "이채영")
+    {
+        cout << name << endl;  // 자신과 같은 MBTI 친구의 이름 출력
+    }
+
+    mbtiFile.close();  // 파일 닫기
+    return 0;
+}
